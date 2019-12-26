341374c973b99a3a334107b360b0ec0d064ca23b
XiNGRZ <chenxingyu92@gmail.com>
2019-12-26 11:33:10 +0800

    m1971: Bring up
    Change-Id: I135cba23efa4e1cdc765ed21dcc0ba70322e6acc

---
+++ device.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2019-2020 The MoKee Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, vendor/meizu/m1971/m1971-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-mokee
+
+# Inherit from sm8150-common
+$(call inherit-product, device/meizu/sm8150-common/common.mk)
